8d911b68475ae5b13a4c566e46c5c912f11747f8
maronicavanessaestellen217q <maronicavanessaestellen217q@hotmail.com>
2024-08-26 15:25:47 +0000

    Initialised

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("uu","uu")
+        ERC721("git commit -m "Initialised"","git push origin main8002ce5db7e2bcc9d9fc5f5bf0ec04fee0136d819dadd6d6a60dea1da830e912")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
